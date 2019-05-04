d0dd2505d4a1e0966d34a3b3d37a219166f2bf49
marina.chernata <chernatikrisha@gmail.com>
2019-05-04 08:42:05 +0300

    New Order

--- static/js/main.js
+++ static/js/main.js
@@ -163,6 +163,353 @@ $(document).ready(function () {
         });
     }
 
+    function forNewOrder() {
+
+        function step1() {
+            function step1GreenBtn() {
+
+                var val_country_ID = $('.new-order-page .block-step-1 select[name=countryID]').val();
+                var val_item_quantity = $('.new-order-page .block-step-1 input[name=item_quantity]').val();
+
+                if((val_country_ID != '') && (val_item_quantity != '')) {
+                    $('.new-order-page .block-step-1 .btn-conf').removeClass('btn-grey').addClass('btn-green');
+                }
+            }
+
+            $('.new-order-page .block-step-1 select[name=countryID]').change(function () {
+                step1GreenBtn();
+            });
+            $('.new-order-page .block-step-1 input[name=item_quantity]').change(function () {
+                step1GreenBtn();
+            });
+
+            $('.new-order-page .block-step-1 .btn-conf').click(function () {
+                var step2 = $('.new-order-page .block-step-2');
+                var step3 = $('.new-order-page .block-step-3');
+                var val_country_ID = $('.new-order-page .block-step-1 select[name=countryID]').val();
+                var val_item_quantity = $('.new-order-page .block-step-1 input[name=item_quantity]').val();
+                var info_msg_error = $('.new-order-page .block-step-1 .block-info-msg.error');
+                var info_msg_confirmed = $('.new-order-page .block-step-1 .block-info-msg.confirmed');
+                var val_item_quantity_int = parseInt(val_item_quantity);
+
+                $.ajax({
+                    type:     "post",
+                    data:     {id: 0},
+                    cache:    false,
+                    url:      "doIt.php",
+                    dataType: "text",
+                    error: function (request, error) {
+                        console.log(arguments);
+                        alert(" Can't do because: " + error);
+
+                        info_msg_error.removeClass('d-none');
+                        info_msg_error.find('span').text(error);
+                    },
+                    success: function () {
+                        info_msg_confirmed.removeClass('d-none');
+                        info_msg_confirmed.find('span').text('Confirmed!');
+
+                        step2.removeClass('d-none');
+                        step3.removeClass('d-none');
+                    }
+                });
+            });
+
+        }
+
+        function step2() {
+            function step2_1() {
+                $('.new-order-page .block-step-2 .tab-pane-1 .btn-search').click(function () {
+                    var val_show_category = $('.new-order-page .block-step-2 .tab-pane-1 select[name=show_category]').val();
+                    var block_pagination_results = $('.new-order-page .block-step-2 .tab-pane-1 .results');
+                    var block_pagination = $('.new-order-page .block-step-2 .tab-pane-1 .block-pagination-main');
+
+                    $.ajax({
+                        type:     "post",
+                        data:     {id: 0},
+                        cache:    false,
+                        url:      "doIt.php",
+                        dataType: "text",
+                        error: function (request, error) {
+                            console.log(arguments);
+                            alert(" Can't do because: " + error);
+                        },
+                        success: function () {
+                            var table_tr = '<tr>' +
+                                '<td>ID8HFE9374</td>' +
+                                '<td>[WILL CONSIDER ADDING STEP TO CODE]</td>' +
+                                '<td>[PRICE]</td>' +
+                                '<td>' +
+                                '<div class="btn btn-submit btn-select selected">Selected</div>' +
+                                '</td>' +
+                                ' <td>' +
+                                '<a href="#" class="btn btn-submit btn-view">' +
+                                '<img src="/static/img/popout.png" srcset="/static/img/popout@2x.png 2x, /static/img/popout@3x.png 3x">' +
+                                '</a>' +
+                                '</td>' +
+                                '</tr>';
+
+                            $('.new-order-page .block-step-2 .tab-pane-1 .table tbody')
+                                .html('')
+                                .append(table_tr);
+
+                            block_pagination_results.text('Results: ' + 6);
+                            block_pagination.find('span').text('Page '+ 1 +' of ' +3);
+                        }
+                    });
+                });
+
+                //Click next button
+                $('.new-order-page .block-step-2 .tab-pane-1 .block-pagination-main .next').click(function () {
+                    var block_pagination_results = $('.new-order-page .block-step-2 .tab-pane-1 .results');
+                    var block_pagination = $('.new-order-page .block-step-2 .tab-pane-1 .block-pagination-main');
+
+                    $.ajax({
+                        type:     "post",
+                        data:     {id: 0},
+                        cache:    false,
+                        url:      "doIt.php",
+                        dataType: "text",
+                        error: function (request, error) {
+                            console.log(arguments);
+                            alert(" Can't do because: " + error);
+                        },
+                        success: function () {
+                            var table_tr = '<tr>' +
+                                '<td>ID8HFE9374</td>' +
+                                '<td>[WILL CONSIDER ADDING STEP TO CODE]</td>' +
+                                '<td>[PRICE]</td>' +
+                                '<td>' +
+                                '<div class="btn btn-submit btn-select selected">Selected</div>' +
+                                '</td>' +
+                                ' <td>' +
+                                '<a href="#" class="btn btn-submit btn-view">' +
+                                '<img src="/static/img/popout.png" srcset="/static/img/popout@2x.png 2x, /static/img/popout@3x.png 3x">' +
+                                '</a>' +
+                                '</td>' +
+                                '</tr>';
+
+                            $('.new-order-page .block-step-2 .tab-pane-1 .table tbody')
+                                .html('')
+                                .append(table_tr);
+
+                            block_pagination_results.text('Results: ' + 6);
+                            block_pagination.find('span').text('Page '+ 1 +' of ' +3);
+                        }
+                    });
+                });
+
+                //Click next prev
+                $('.new-order-page .block-step-2 .tab-pane-1 .block-pagination-main .prev').click(function () {
+                    var block_pagination_results = $('.new-order-page .block-step-2 .tab-pane-1 .results');
+                    var block_pagination = $('.new-order-page .block-step-2 .tab-pane-1 .block-pagination-main');
+
+                    $.ajax({
+                        type:     "post",
+                        data:     {id: 0},
+                        cache:    false,
+                        url:      "doIt.php",
+                        dataType: "text",
+                        error: function (request, error) {
+                            console.log(arguments);
+                            alert(" Can't do because: " + error);
+                        },
+                        success: function () {
+                            var table_tr = '<tr>' +
+                                '<td>ID8HFE9374</td>' +
+                                '<td>[WILL CONSIDER ADDING STEP TO CODE]</td>' +
+                                '<td>[PRICE]</td>' +
+                                '<td>' +
+                                '<div class="btn btn-submit btn-select selected">Selected</div>' +
+                                '</td>' +
+                                ' <td>' +
+                                '<a href="#" class="btn btn-submit btn-view">' +
+                                '<img src="/static/img/popout.png" srcset="/static/img/popout@2x.png 2x, /static/img/popout@3x.png 3x">' +
+                                '</a>' +
+                                '</td>' +
+                                '</tr>';
+
+                            $('.new-order-page .block-step-2 .tab-pane-1 .table tbody')
+                                .html('')
+                                .append(table_tr);
+
+                            block_pagination_results.text('Results: ' + 6);
+                            block_pagination.find('span').text('Page '+ 1 +' of ' +3);
+                        }
+                    });
+                });
+
+            }
+
+            function step2_2() {
+                $('.new-order-page .block-step-2 .tab-pane-2 .btn-search').click(function () {
+                    var val_search_category = $('.new-order-page .block-step-2 .tab-pane-2 select[name=search_category]').val();
+                    var val_search_text = $('.new-order-page .block-step-2 .tab-pane-2 input[name=search_text]').val();
+                    var block_pagination_results = $('.new-order-page .block-step-2 .tab-pane-2 .results');
+                    var block_pagination = $('.new-order-page .block-step-2 .tab-pane-2 .block-pagination-main');
+
+                    $.ajax({
+                        type:     "post",
+                        data:     {id: 0},
+                        cache:    false,
+                        url:      "doIt.php",
+                        dataType: "text",
+                        error: function (request, error) {
+                            console.log(arguments);
+                            alert(" Can't do because: " + error);
+                        },
+                        success: function () {
+                            var table_tr = '<tr>' +
+                                '<td>ID8HFE9374</td>' +
+                                '<td>[WILL CONSIDER ADDING STEP TO CODE]</td>' +
+                                '<td>[PRICE]</td>' +
+                                '<td>' +
+                                '<div class="btn btn-submit btn-select selected">Selected</div>' +
+                                '</td>' +
+                                ' <td>' +
+                                '<a href="#" class="btn btn-submit btn-view">' +
+                                '<img src="/static/img/popout.png" srcset="/static/img/popout@2x.png 2x, /static/img/popout@3x.png 3x">' +
+                                '</a>' +
+                                '</td>' +
+                                '</tr>';
+
+                            $('.new-order-page .block-step-2 .tab-pane-2 .table tbody')
+                                .html('')
+                                .append(table_tr);
+
+                            block_pagination_results.text('Results: ' + 6);
+                            block_pagination.find('span').text('Page '+ 1 +' of ' +3);
+                        }
+                    });
+                });
+
+                //Click next button
+                $('.new-order-page .block-step-2 .tab-pane-2 .block-pagination-main .next').click(function () {
+                    var block_pagination_results = $('.new-order-page .block-step-2 .tab-pane-2 .results');
+                    var block_pagination = $('.new-order-page .block-step-2 .tab-pane-2 .block-pagination-main');
+
+                    $.ajax({
+                        type:     "post",
+                        data:     {id: 0},
+                        cache:    false,
+                        url:      "doIt.php",
+                        dataType: "text",
+                        error: function (request, error) {
+                            console.log(arguments);
+                            alert(" Can't do because: " + error);
+                        },
+                        success: function () {
+                            var table_tr = '<tr>' +
+                                '<td>ID8HFE9374</td>' +
+                                '<td>[WILL CONSIDER ADDING STEP TO CODE]</td>' +
+                                '<td>[PRICE]</td>' +
+                                '<td>' +
+                                '<div class="btn btn-submit btn-select selected">Selected</div>' +
+                                '</td>' +
+                                ' <td>' +
+                                '<a href="#" class="btn btn-submit btn-view">' +
+                                '<img src="/static/img/popout.png" srcset="/static/img/popout@2x.png 2x, /static/img/popout@3x.png 3x">' +
+                                '</a>' +
+                                '</td>' +
+                                '</tr>';
+
+                            $('.new-order-page .block-step-2 .tab-pane-2 .table tbody')
+                                .html('')
+                                .append(table_tr);
+
+                            block_pagination_results.text('Results: ' + 6);
+                            block_pagination.find('span').text('Page '+ 1 +' of ' +3);
+                        }
+                    });
+                });
+
+                //Click next prev
+                $('.new-order-page .block-step-2 .tab-pane-2 .block-pagination-main .prev').click(function () {
+                    var block_pagination_results = $('.new-order-page .block-step-2 .tab-pane-2 .results');
+                    var block_pagination = $('.new-order-page .block-step-2 .tab-pane-2 .block-pagination-main');
+
+                    $.ajax({
+                        type:     "post",
+                        data:     {id: 0},
+                        cache:    false,
+                        url:      "doIt.php",
+                        dataType: "text",
+                        error: function (request, error) {
+                            console.log(arguments);
+                            alert(" Can't do because: " + error);
+                        },
+                        success: function () {
+                            var table_tr = '<tr>' +
+                                '<td>ID8HFE9374</td>' +
+                                '<td>[WILL CONSIDER ADDING STEP TO CODE]</td>' +
+                                '<td>[PRICE]</td>' +
+                                '<td>' +
+                                '<div class="btn btn-submit btn-select selected">Selected</div>' +
+                                '</td>' +
+                                ' <td>' +
+                                '<a href="#" class="btn btn-submit btn-view">' +
+                                '<img src="/static/img/popout.png" srcset="/static/img/popout@2x.png 2x, /static/img/popout@3x.png 3x">' +
+                                '</a>' +
+                                '</td>' +
+                                '</tr>';
+
+                            $('.new-order-page .block-step-2 .tab-pane-2 .table tbody')
+                                .html('')
+                                .append(table_tr);
+
+                            block_pagination_results.text('Results: ' + 6);
+                            block_pagination.find('span').text('Page '+ 1 +' of ' +3);
+                        }
+                    });
+                });
+            }
+            function step2_3() {
+                $('.new-order-page .block-step-2 .tab-pane-3 .btn-search').click(function () {
+                    var val_asin_search = $('.new-order-page .block-step-2 .tab-pane-3 input[name=asin_search]').val();
+
+                    $.ajax({
+                        type:     "post",
+                        data:     {id: 0},
+                        cache:    false,
+                        url:      "doIt.php",
+                        dataType: "text",
+                        error: function (request, error) {
+                            console.log(arguments);
+                            alert(" Can't do because: " + error);
+                        },
+                        success: function () {
+                            var table_tr = '<tr>' +
+                                '<td>ID8HFE9374</td>' +
+                                '<td>[WILL CONSIDER ADDING STEP TO CODE]</td>' +
+                                '<td>[PRICE]</td>' +
+                                '<td>' +
+                                '<div class="btn btn-submit btn-select selected">Selected</div>' +
+                                '</td>' +
+                                ' <td>' +
+                                '<a href="#" class="btn btn-submit btn-view">' +
+                                '<img src="/static/img/popout.png" srcset="/static/img/popout@2x.png 2x, /static/img/popout@3x.png 3x">' +
+                                '</a>' +
+                                '</td>' +
+                                '</tr>';
+
+                            $('.new-order-page .block-step-2 .tab-pane-3 .table tbody')
+                                .html('')
+                                .append(table_tr);
+                        }
+                    });
+                });
+            }
+
+            step2_1();
+            step2_2();
+            step2_3();
+        }
+
+        step1();
+        step2();
+
+    }
+
     function heightForElements (elements, maxHeight) {
         elements.css('height', 'auto');
         elements.each(function( index ) {
@@ -214,4 +561,6 @@ $(document).ready(function () {
     activeOrder();
     uploadImg();
     forPaymentPage();
+
+    forNewOrder();
 });
